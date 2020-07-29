d1cb98699189f17360e20ec57e3d3d16d6085cf5
ed-commits <edward.alexander.cant@gmail.com>
2020-07-29 14:03:53 +0100

    abstract vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,19 @@
-public class Vehicle {
+public abstract class Vehicle {
+
+    private String name;
+    private Double price;
+
+    public Vehicle(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
