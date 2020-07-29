259f77f25068759c0f17ad485c511b0084738aaa
ed-commits <edward.alexander.cant@gmail.com>
2020-07-29 13:58:21 +0100

    create files

---
+++ src/test/java/Runner.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class Runner {
+    @Before
+    public void setup() {
+        System.out.println("Test");
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
