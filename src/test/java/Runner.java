eb8b3568eac43dc6dc150244cedbb4b0c91e3ff9
ed-commits <edward.alexander.cant@gmail.com>
2020-07-29 14:10:40 +0100

    test the vehicle accessors on Car

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,14 +1,30 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class Runner {
+    Car car;
+
     @Before
     public void setup() {
         System.out.println("Test");
+
+        car = new Car("Toyota", 6130.00);
     }
 
     @Test
     public void test() {
 
+    }
+
+    @Test
+    public void testCarGetName() {
+        assertEquals("Toyota", car.getName());
+    }
+
+    @Test
+    public void testCarGetPrice() {
+        assertEquals(6130.00, car.getPrice(), 0.00001);
     }
 }
