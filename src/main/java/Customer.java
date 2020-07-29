1676a1c2dd76ffeaab7bc847453c8468da43f0e2
ed-commits <edward.alexander.cant@gmail.com>
2020-07-29 14:26:12 +0100

    constructors for Dealership and Customer

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private Double wallet;
+    private ArrayList<Vehicle> garage;
+
+    public Customer(Double wallet) {
+        this.wallet = wallet;
+        this.garage = new ArrayList<Vehicle>();
+    }
+
+    public Double getWallet() {
+        return wallet;
+    }
+
+    public ArrayList<Vehicle> getGarage() {
+        return garage;
+    }
 }
--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Dealership {
+    private Double till;
+    private ArrayList<Vehicle> stock;
+
+    public Dealership(Double till) {
+        this.till = till;
+        this.stock = new ArrayList<Vehicle>();
+    }
+
+    public Double getTill() {
+        return till;
+    }
+
+    public ArrayList<Vehicle> getStock() {
+        return stock;
+    }
 }
